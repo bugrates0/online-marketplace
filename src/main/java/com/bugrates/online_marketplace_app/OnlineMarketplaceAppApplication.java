0506018fcb6d34d7c8ee4e8cfc8c66ce1cdde15d
Bugra Ates <bugrates25@gmail.com>
2025-01-28 15:25:15 +0300

    initial commit

---
+++ src/main/java/com/bugrates/online_marketplace_app/OnlineMarketplaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bugrates.online_marketplace_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketplaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMarketplaceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugrates/online_marketplace_app/OnlineMarketplaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugrates.online_marketplace_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketplaceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
